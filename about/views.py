7950a77e3d073fa80cb31eccacb785c0bd23f61c
Freddy Larsson <professoro88@gmail.com>
2024-05-30 12:08:59 +0000

    add about app

--- about/views.py
+++ about/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def about_me(request):
+    return HttpResponse("<h1>This would be the about page</h1>")
